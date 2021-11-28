3ea672af07656625aa23d17f15d9d985d5fa97d2
facelessmindz <fuzzzzzz@gmail.com>
2021-11-28 05:43:02 +0100

    Adding batch file for ZBrush 2022

---
+++ batch/upgrade-dev-tools-2022.bat
@@ -0,0 +1,4 @@
+:: dev tools
+
+echo Upgrade DEV TOOLS Plugin in Zbrush 2021\ZStartup\Zplug64 directory
+xcopy /s /y "L:\programming\WORKSPACES\zbrush-dev-tools\PuppetMaster_DevTools.txt" "D:\Imaging\Pixologic\ZBrush 2022\ZStartup\ZPlugs64\" /f
